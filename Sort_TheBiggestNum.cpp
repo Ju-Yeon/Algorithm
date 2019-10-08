12fcc2984bdff8d4ea409b360243e02a17af9cad
Ju-Yeon Park <pjuyeon25@gmail.com>
2019-10-08 16:33:06 +0900

    Sort / TheBiggestNum / use comp function / not solved yet

---
+++ Sort_TheBiggestNum.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+bool comp  (int a,  int b);
+
+string solution(vector<int> numbers) {
+    
+    string answer = "";
+    sort(numbers.begin(),numbers.end(),comp);
+    if(numbers[0]==0)return "0";
+    
+    for(int i=0;i<numbers.size(); i++)
+        answer = answer + to_string(numbers[i]);
+    
+    string answers = "";
+    answers=to_string(stoi(answer));
+    return answers;
+}
+
+
+ bool comp  (int a,  int b)
+{
+    string aa = to_string(a);
+    string bb = to_string(b);
+    
+   bool result = (stoi(string(aa+bb))<stoi(string(bb+aa)))? false: true;
+     
+    return result;
+     
+}
