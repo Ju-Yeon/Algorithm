11d41ea5888974ed29ce80a8704ae85ac8def7ec
Ju-Yeon Park <pjuyeon25@gmail.com>
2020-03-19 17:36:32 +0900

    not solved yet exception test1

--- Programmers/holiday_traffic.cpp
+++ Programmers/holiday_traffic.cpp
@@ -4,43 +4,57 @@
 
 using namespace std;
 int solution(vector<string> lines);
-double* getStime(double etime[3], double time);
-bool isSmall(double small[3], double big[3]);
+int* getStime(int etime[3], int time);
+bool isSmall(int small[3], int big[3]);
 
 struct transaction {
-    double * stime;
-    double * etime;
+    int * stime;
+    int * etime;
 };
 
-//2016-09-15 21:00:02.066 2.62s
+
 int solution(vector<string> lines) {
     int answer = 0;
     transaction * t = new transaction[lines.size()];
 
     for(int i = 0; i < lines.size(); i++){
-        t[i].stime = new double[3];
-        t[i].etime = new double[3];
+        t[i].stime = new int[3];
+        t[i].etime = new int[3];
 
         for(int l = 0; l < 2; l++)
-            t[i].etime[l] = atof(lines[i].substr(11 + 3 * l, 2).c_str());
-        t[i].etime[2] = atof(lines[i].substr(17, 6).c_str());
-        t[i].stime = getStime(t[i].etime, atof(lines[i].substr(24, lines[i].find("s")-24).c_str()));
+            t[i].etime[l] = atoi(lines[i].substr(11 + 3 * l, 2).c_str());
+        t[i].etime[2] = atof(lines[i].substr(17, 6).c_str()) * 1000.0;
+
+        t[i].stime = getStime(t[i].etime, atof(lines[i].substr(24, lines[i].find("s")-24).c_str()) * 1000.0);
     }
 
-    double *stime = new double[3];
-    double *etime = new double[3];
+//    for(int i = 0; i < lines.size(); i++) {
+//        for (int a = 0; a < 3; a++) {
+//            cout << t[i].stime[a] << " ";
+//        }
+//        cout << endl;
+//        for (int a = 0; a < 3; a++) {
+//            cout << t[i].etime[a] << " ";
+//        }
+//        cout << endl << endl;
+//    }
+
+    int *stime = new int[3];
+    int *etime = new int[3];
     int count = 0;
 
     for(int i = 0; i < lines.size(); i++){
         count = 0;
-        stime = t[i].etime;
-        etime = t[i].etime;
-        etime[2] += 1.000;
+        for(int l = 0; l<3; l++){
+            stime[l] = t[i].etime[l];
+            etime[l] = t[i].etime[l];
+        }
+
+        etime[2] = etime[2] + 1000 - 1;
 
         for(int l = i; l < lines.size(); l++){
              if(!isSmall(t[l].etime, stime) && !isSmall(etime, t[l].stime)) {
                  count ++;
-                 cout<<i<<" "<<l<<endl;
              }
         }
         answer = answer < count ? count : answer;
@@ -49,15 +63,14 @@ int solution(vector<string> lines) {
     return answer;
 }
 
-double* getStime(double etime[3], double time){
-    double *result = new double[3];
-    double flag = 0.0;
+int* getStime(int etime[3], int time){
+    int *result = new int[3];
 
     if(etime[2] > time){
         for(int i = 0; i < 2; i++){
             result[i] = etime[i];
         }
-        result[2] = etime[2] - time + 0.001;
+        result[2] = etime[2] - time + 1;
 
     }else{
         if(result[1] != 0){
@@ -67,18 +80,21 @@ double* getStime(double etime[3], double time){
             result[0] = etime[0]-1;
             result[1] = 59.0;
         }
-        result[2] = 60 - (time - etime[2] - 0.001);
+        result[2] = 60000 - (time - etime[2] - 1);
     }
 
     return result;
 }
 
-bool isSmall(double small[3], double big[3]){
-    for(int i = 0; i<2; i++){
-        if(small[i] > big[i]) return false;
-    }
-    if(small[2] >= big[2]) return false;
-    else return true;
+bool isSmall(int small[3], int big[3]){
+    if(small[0] < big[0])return true;
+    else if(small[0] > big[0])return false;
+
+    if(small[1] < big[1]) return true;
+    else if(small[1] > big[1])return false;
+
+    if(small[2] < big[2]) return true;
+    else if(small[2] >= big[2]) return false;
 }
 
 int main(){
@@ -94,6 +110,9 @@ int main(){
             "2016-09-15 21:00:00.966 0.381s",
             "2016-09-15 21:00:02.066 2.62s"
     };
+
+    vector<string> lines1 = {"2016-09-15 01:00:04.001 2.0s","2016-09-15 01:00:07.000 2s"};//{"2016-09-15 01:00:04.002 2.0s", "2016-09-15 01:00:07.000 2s"};
+
 
     cout<<solution(lines)<<endl;
     return 0;
