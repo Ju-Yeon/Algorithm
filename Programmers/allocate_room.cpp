45999b1717acf26b7e794c999845e8a3fec123db
Ju-Yeon Park <pjuyeon25@gmail.com>
2020-04-06 23:12:23 +0900

    use disjoint find..but not solved it  about effeciency

--- Programmers/allocate_room.cpp
+++ Programmers/allocate_room.cpp
@@ -1,19 +1,39 @@
-#include <string>
 #include <vector>
-#include <set>
+#include <map>
 #include <iostream>
 
 using namespace std;
+map <long long, long long> m;
+
+long long find(long long key){
+
+    if(!m[key]) return key;
+    return find(m[key]+1);
+
+}
 
 vector<long long> solution(long long k, vector<long long> room_number) {
     vector<long long> answer;
 
-    set<int> s;
-    for(int i = 0, l; i < room_number.size(); i++){
-        l = room_number[i];
-        while(!s.insert(l).second){l++;}
-        answer.push_back(l);
-    }
+    for(int i = 0; i < room_number.size(); i++){
 
+        long long n = find(room_number[i]);
+        
+        //다음 인덱스 고려
+        if(!m[n+1]) m[n] = n;
+        else m[n] = n + 1;;
+
+        //이전 인덱스 고려
+        if(m[n-1]) m[n-1] = n;
+
+        answer.push_back(n);
+
+    }
+    for(int i =0 ; i < answer.size(); i++)
+        cout<<answer[i]<<endl;
     return answer;
+}
+
+int main(){
+    solution(10, {1,3,4,1,3,1});
 }
