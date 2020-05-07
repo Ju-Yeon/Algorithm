482e804db7cdc19abfd25f4511ddf6beed1af884
Ju-Yeon Park <pjuyeon25@gmail.com>
2020-05-07 16:32:22 +0900

    solved it with check before load

--- SWEA/1949.cpp
+++ SWEA/1949.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<cstring>
 #include<vector>
+#include<set>
 
 using namespace std;
 
@@ -8,8 +10,10 @@ int map[8][8];
 int map_fixed[8][8];
 int dx[] = {0, +1, 0, -1};
 int dy[] = {+1, 0, -1, 0};
+vector <pair<int, int>> load;
 vector<pair<int, int>> highest;
 void dfs(int y, int x, int k, int len);
+int isChecked(int y, int x);
 
 int main(int argc, char** argv)
 {
@@ -24,6 +28,7 @@ int main(int argc, char** argv)
         totalLen = 0;
         memset(map, 0, 8);
         memset(map_fixed, 0, 8);
+        load.clear();
 
         for(int i = 0; i < N; i++){
             for(int l = 0; l < N; l++){
@@ -40,8 +45,9 @@ int main(int argc, char** argv)
         }
 
         for(int i = 0; i < highest.size(); i++) {
+            load.emplace_back(make_pair(highest[i].first, highest[i].second));
             dfs(highest[i].first, highest[i].second, k, 1);
-            //cout<<totalLen<<endl;
+            load.pop_back();
         }
         cout<<"#"<<test_case<<" "<<totalLen<<endl;
     }
@@ -50,32 +56,46 @@ int main(int argc, char** argv)
 
 void dfs(int y, int x, int k, int len) {
 
+
     int flag = 0;
     for (int i = 0; i < 4; i++) {
         int nx = x + dx[i];
         int ny = y + dy[i];
-        if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+        if(nx < 0 || ny < 0 || nx >= N || ny >= N ) continue;
 
-        if (map[y][x] > map[ny][nx]) {
+        if (map[y][x] > map[ny][nx] && isChecked(ny, nx)) {
+            load.push_back(make_pair(ny, nx));
             dfs(ny, nx, k, len + 1);
             flag++;
+            load.pop_back();
         }
 
         for (int t = 1; t <= k; t++) {
             if (map_fixed[ny][nx]-t < 0) continue;
             map[ny][nx] = map_fixed[ny][nx] - t;
 
-            if (map[y][x] > map[ny][nx]) {
-                //cout<<ny<<nx<<map[ny][nx]<<len<<endl;
+            if (map[y][x] > map[ny][nx] && isChecked(ny, nx)){
+                load.push_back(make_pair(ny, nx));
                 dfs(ny, nx, 0, len + 1);
                 flag++;
+                load.pop_back();
             }
             map[ny][nx] = map_fixed[ny][nx];
+
         }
     }
-
+    //더이상 이동하지 않으면 종료
     if(flag == 0){
         if (totalLen < len) totalLen = len;
         return;
     }
+}
+
+//경로 방문 유뮤
+int isChecked(int y, int x){
+    for(int i = 0; i < load.size(); i++) {
+        if (load[i].first == y && load[i].second == x)
+            return 0;
+    }
+    return 1;
 }
