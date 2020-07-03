93cbcfdd175f0bc92a5f2082e4111fd8e9cdc8b3
Ju-Yeon Park <pjuyeon25@gmail.com>
2020-07-03 21:22:08 +0900

    try quick sort, but..

--- Sort/Sort_TheBiggestNum.cpp
+++ Sort/Sort_TheBiggestNum.cpp
@@ -1,47 +1,65 @@
-//가장 큰 수 https://www.welcomekakao.com/learn/courses/30/lessons/42746
-
 #include <string>
 #include <vector>
 #include <iostream>
 #include<algorithm>
-
 using namespace std;
-
-/*
+vector<int>numbers;
 bool comp  (int a,  int b);
+void quickSort( int s, int e);
 
-string solution(vector<int> numbers) {
-    
+string solution(vector<int> number) {
+
+    numbers = number;
     string answer = "";
-    sort(numbers.begin(),numbers.end(),comp);
+    quickSort(0,numbers.size()-1);
+
     if(numbers[0]==0)return "0";
-    
+
     for(int i=0;i<numbers.size(); i++)
         answer = answer + to_string(numbers[i]);
-    
+
     string answers = "";
     answers=to_string(stoi(answer));
     return answers;
 }
 
 
- bool comp  (int a,  int b)
+bool comp  (int a,  int b)
 {
     string aa = to_string(a);
     string bb = to_string(b);
-    
-   bool result = (stoi(string(aa+bb))<stoi(string(bb+aa)))? false: true;
-     
-    return result;
-     
-}
-*/
 
-string solution(vector<int> numbers) {
-    
-    string answer = "";
-    
-    
-    
-    return;
+    return (stoi(string(aa+bb))>stoi(string(bb+aa)));
+
+}
+
+void quickSort( int s, int e){
+    if(s+1 >= e) return;
+
+    int p = (s + e)/2;
+    int pivot = numbers[p];
+    int l = s;
+    int r = e;
+    int temp;
+
+    while(l < r){
+        while(comp(numbers[l], pivot) && l<e)
+            l++;
+        while(comp(pivot,numbers[r]) && s<r)
+            r--;
+
+        if(l<r) { //swap
+            temp = numbers[l];
+            numbers[l] = numbers[r];
+            numbers[r] = temp;
+        }
+    }
+    quickSort(l, e);
+    quickSort(s, r);
+
+}
+
+int main(){
+    cout<<solution({3, 30})<<endl;
+
 }
