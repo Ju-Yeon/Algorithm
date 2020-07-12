7d11ee9ce79d0fbf4fc85b325b972bf79c2eaee5
Ju-Yeon Park <pjuyeon25@gmail.com>
2020-07-12 22:13:52 +0900

    finally solved! type problem

--- Sort/Sort_TheBiggestNum.cpp
+++ Sort/Sort_TheBiggestNum.cpp
@@ -1,65 +1,27 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include<algorithm>
+#include <algorithm>
+
 using namespace std;
-vector<int>numbers;
-bool comp  (int a,  int b);
-void quickSort( int s, int e);
-
-string solution(vector<int> number) {
-
-    numbers = number;
-    string answer = "";
-    quickSort(0,numbers.size()-1);
-
-    if(numbers[0]==0)return "0";
-
-    for(int i=0;i<numbers.size(); i++)
-        answer = answer + to_string(numbers[i]);
-
-    string answers = "";
-    answers=to_string(stoi(answer));
-    return answers;
-}
-
-
-bool comp  (int a,  int b)
+bool comp  (const string &a, const string &b)
 {
-    string aa = to_string(a);
-    string bb = to_string(b);
-
-    return (stoi(string(aa+bb))>stoi(string(bb+aa)));
-
+    return (a+b) > (b+a);
 }
 
-void quickSort( int s, int e){
-    if(s+1 >= e) return;
+string solution(vector<int> numbers) {
 
-    int p = (s + e)/2;
-    int pivot = numbers[p];
-    int l = s;
-    int r = e;
-    int temp;
+    string answer = "";
+    
+    vector<string> number;
+    for(int i = 0; i < numbers.size(); i++)
+        number.push_back(to_string(numbers[i]));
 
-    while(l < r){
-        while(comp(numbers[l], pivot) && l<e)
-            l++;
-        while(comp(pivot,numbers[r]) && s<r)
-            r--;
+    sort(number.begin(), number.end(), comp);
+    if(number.front()=="0")return "0";
 
-        if(l<r) { //swap
-            temp = numbers[l];
-            numbers[l] = numbers[r];
-            numbers[r] = temp;
-        }
-    }
-    quickSort(l, e);
-    quickSort(s, r);
-
-}
-
-int main(){
-    cout<<solution({3, 30})<<endl;
-
+    for(int i = 0; i < number.size(); i++)
+        answer += number[i];
+    
+    //string answers = to_string(stoi(answer));
+    return answer;
 }
