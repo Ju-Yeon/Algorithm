fc31faaefde0f4d16967db1c60cf1ad5d9966f05
Ju-Yeon Park <pjuyeon25@gmail.com>
2021-01-03 16:25:14 +0900

    should know about bfs and dfs, and finally solved it

--- Baekjoon/15586_mooTube.cpp
+++ Baekjoon/15586_mooTube.cpp
@@ -1,66 +1,57 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 int N, Q;
-vector <vector <int>> v;
-vector <int> dx;
+vector <pair<int, int>> v[5001];
 
-int solution(int target, int currentPos, int currentValue, vector<int> check){
+int bfs(int k, int n){
+    int result = 0;
+    vector<bool> check(5001, false);
+    queue<int> q;
 
-    int value = v[currentPos][target];
-    if(v[currentPos][target] != 0) return currentValue == 0 || value < currentValue? value: currentValue;
-    check[currentPos] = 1;
+    q.push(n);
+    check[n] = true;
 
-    for(int i = 1; i < N; i ++){
-        value = v[currentPos][(currentPos+dx[i])%4];
-        if(check[(currentPos+dx[i])%4] == 1 || value == 0) continue;
-        return solution(target, (currentPos+dx[i])%4, currentValue == 0 || value < currentValue? value: currentValue, check);
+    while(!q.empty()){
+        int current = q.front();
+        q.pop();
+
+        for(int i = 0; i < v[current].size(); i++){
+            int next = v[current][i].first;
+            int value = v[current][i].second;
+
+            if(!check[next] && k <= value){
+                check[next] = true;
+                q.push(next);
+                result++;
+            }
+        }
     }
-
+    return result;
 }
 
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     cin >> N >> Q;
-    vector <int> vSub (N, 0);
-    for(int i = 0 ; i < N; i++){
-        v.push_back(vSub);
-        dx.push_back(i);
+
+    int p,q,r,k,n;
+    for(int i = 0; i < N-1; i++){
+        cin>>p>>q>>r;
+        v[p].push_back({q,r});
+        v[q].push_back({p,r});
+
     }
-
-
-    string str;
-    vector <string> cmd;
-
-    int n = 0;
-    cin.ignore();
-    while(true){
-        str.empty();
-        getline(cin, str);
-        cmd.push_back(str);
-        if(str.length() == 3 && ++n == Q)break;
+    for(int i = 0; i < Q; i++){
+        cin>>k>>n;
+        cout<<bfs(k, n)<<endl;
     }
-
-    for(int i = 0; i < cmd.size() - Q; i++){
-        v[cmd[i][0]-49][cmd[i][2]-49] = cmd[i][4]-48;
-        v[cmd[i][2]-49][cmd[i][0]-49] = cmd[i][4]-48;
-    }
-
-    vector<int> check(N, 0);
-    for(int i = cmd.size()-Q; i < cmd.size(); i++){
-        n = 0;
-        for(int l = 0; l < N; l ++) {
-            if(cmd[i][2]-49 == l) continue;
-
-            fill(check.begin(),check.end(),0);
-            v[cmd[i][2]-49][l] = solution(l,cmd[i][2]-49, 0, check);
-            v[l][cmd[i][2]-49] = v[cmd[i][2]-49][l];
-
-            if(cmd[i][0]-48 <= solution(l,cmd[i][2]-49, 0, check))n++;
-        }
-        cout << n << endl;
-    }
+    return 0;
+    
 }
