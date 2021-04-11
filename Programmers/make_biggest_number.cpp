21875ebd6233ab32c309b89bc2baa8da1668e00e
Ju-Yeon Park <pjuyeon25@gmail.com>
2021-04-11 20:02:25 +0900

    modify logic and solved it with greedy method

--- Programmers/make_biggest_number.cpp
+++ Programmers/make_biggest_number.cpp
@@ -1,3 +1,5 @@
+
+
 #include <string>
 #include <vector>
 #include <iostream>
@@ -6,32 +8,21 @@ using namespace std;
 string solution(string number, int k) {
     string s = "";
 
-    int i;
-    for(i = 0; i < number.length(); i++){
-        if(k == 0){
-            break;
-        }
-
+    for(int i = 0; i < number.length(); i++){
+        
         //string 에서 한자리 숫자 비교가능(사전순서대로기 때문), 그러나 여러자리를 비교할 때는 형변환 필요
         while (s.length() != 0 && number[i] > s.back() && k > 0) {
-            s = s.substr(0, s.length() - 1);
+            s.pop_back();
             k--;
         }
-        /*  if(k == 0){
+        if(k == 0){
             s += number.substr(i);
             break;
-        }*/
-        s += number[i];
+        }
+        s.push_back(number[i]);
     }
-
-
-    if(i != number.length()) {
-         cout<<s+number.substr(i)<<endl;
-         return s+number.substr(i);
-    } else {
-        cout<<s<<endl;
-        return s;
-    }
+    cout<<s<<endl;
+    return s.substr(0,s.length() - k);
 }
 
 int main(){
